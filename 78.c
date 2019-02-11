49fa4ea5381f3acb3b4777a9d0242bdf3d014967
harudinash <46379892+harudinash@users.noreply.github.com>
2019-02-11 10:07:49 +0530

    Create 78.c

---
+++ 78.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main()
+{
+    int a,x=0,y,z,i;
+    //clrscr();
+    printf("Input :");
+    scanf("%d",&a);
+    printf("Output :\n");
+   
+       if(a%13==0)
+       printf("yes ");
+       else
+       printf("No");
+   
+    return 0;
+}
