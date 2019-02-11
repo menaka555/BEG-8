76fdaf8cc7585f3f1be1ed7bb96099107fd11768
harudinash <46379892+harudinash@users.noreply.github.com>
2019-02-11 22:32:58 +0530

    Create 95.c

---
+++ 95.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main()
+{
+    // int a;
+    float a,c=0,d,i,e,f=0;
+   
+    printf("Input :\n");
+    scanf("%f%f%f",&a,&d,&c);
+   
+   printf("Output :\n");
+   f=a/100;
+  // printf("a=%f",f);
+   i=d*c;
+   e=f*i;
+   
+   printf("\n%f",e);
+    return 0;
+}
