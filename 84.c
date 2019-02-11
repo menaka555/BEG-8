8ca261d1e080a14cbfc0a41d487e867d66bddcba
harudinash <46379892+harudinash@users.noreply.github.com>
2019-02-11 07:38:55 +0530

    Create 84.c

---
+++ 84.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main()
+{
+    int a,b,c=0,d=0,i;
+  //  char d[100000];
+    printf("Input :");
+scanf("%d",&a);
+    printf("Output :\n");
+    a=a+1;
+    printf("%d",a);
+    
+   
+    return 0;
+}
