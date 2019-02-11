a8918bf170db43433fea8c1636a23974d7521b1d
harudinash <46379892+harudinash@users.noreply.github.com>
2019-02-11 22:56:03 +0530

    Create 99.c

---
+++ 99.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main()
+{
+    // int a;
+    int a,c=0,d[100000],b,i,e,f=0;
+   
+    printf("Input :\n");
+    scanf("%d%d%d",&a,&b,&c);
+   
+   printf("Output :\n");
+ e=a*b;
+ i=e%c;
+ printf("%d",i);
+    return 0;
+}
