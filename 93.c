8e25e374958dfbd0bc9748f9156fc1deb7f64e7a
harudinash <46379892+harudinash@users.noreply.github.com>
2019-02-11 22:11:40 +0530

    Create 93.c

---
+++ 93.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main()
+{
+    int a,b[100000],c=0,d,i,e;
+    printf("Input :");
+    scanf("%d",&a);
+   printf("Output :\n");
+   c=a+273;
+   
+   printf("\n%d",c);
+    return 0;
+}
